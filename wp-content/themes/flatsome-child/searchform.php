fe222eb7be4999f7da8ea249b2d854cdef26407b
michaelh818 <michaelharootoonyan@gmail.com>
2019-09-13 11:42:07 -0700

    did not fix / check 188

--- wp-content/themes/flatsome-child/searchform.php
+++ wp-content/themes/flatsome-child/searchform.php
@@ -11,7 +11,8 @@ if(get_theme_mod('search_placeholder')) $placeholder = get_theme_mod('search_pla
 <form method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<div class="flex-row relative">
 			<div class="flex-col flex-grow">
-	   	   <input type="search" class="search-field mb-0" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo $placeholder; ?>" />
+				<label style="display:none;" for="s">Search</label>
+				<input type="search" class="search-field mb-0" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo $placeholder; ?>" />
 			</div><!-- .flex-col -->
 			<div class="flex-col">
 				<button type="submit" class="ux-search-submit submit-button secondary button icon mb-0">
