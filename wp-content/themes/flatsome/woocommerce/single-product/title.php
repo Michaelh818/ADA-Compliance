009a5689840d705c83921e2f5b720093d2576077
michaelh818 <michaelharootoonyan@gmail.com>
2019-09-13 16:27:54 -0700

    fix / check 37 and 38

--- wp-content/themes/flatsome/woocommerce/single-product/title.php
+++ wp-content/themes/flatsome/woocommerce/single-product/title.php
@@ -10,11 +10,12 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
+// todo: this used to be h1 so you need to add a class to your h3 tag to make it look like h1 
 ?>
-<h1 class="product-title product_title entry-title">
+
+<h3 class="product-title product_title entry-title">
 	<?php the_title(); ?>
-</h1>
+</h3>
 
 <?php if(get_theme_mod('product_title_divider', 1)) { ?>
 	<div class="is-divider small"></div>
